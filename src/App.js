13697140cc1fd0df94eba66bcd6c6048335bb3c6
RGameShow <info@roman-web-developer.ru>
2022-12-27 14:20:22 +0300

    Full project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CostItem from "./components/CostItem";
+function App() {
+  return (
+    <div>
+        <h1>Hi!</h1>
+        <CostItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CostItem.js
@@ -0,0 +1,10 @@
+function CostItem() {
+    return (
+        <div>
+            <p>Date: <span>01.03.1996</span></p>
+            <p>Title: <span>Food</span></p>
+            <p>Sum: <span>250</span></p>
+        </div>
+    );
+}
+export default CostItem;
