f1816dde872a5d068ad3545a411570b54a50dce1
RGameShow <info@roman-web-developer.ru>
2022-12-27 16:24:17 +0300

    Process

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import CostItem from "./components/CostItem";
+import CostItem from "./components/Costs/CostItem";
+import Card from "./components/UI/Card";
 function App() {
-  return (
-    <div>
-        <h1>Hi!</h1>
-        <CostItem/>
-    </div>
-  );
+    const costs =[
+        {
+            costDate: new Date(2021, 2, 12),
+            costDesc: 'Холодильник',
+            costAmount: 999.99,
+        },
+        {
+            costDate: new Date(2021, 2, 15),
+            costDesc: 'Джинсы',
+            costAmount: 58,
+        },
+        {
+            costDate: new Date(2021, 2, 17),
+            costDesc: 'Футболка',
+            costAmount: 25,
+        },
+    ]
+
+    return (
+        <div>
+            <h1>Hi!</h1>
+            <Card className="costs">
+                {
+                    costs.map( (c, k) => <CostItem
+                        costDate={c.costDate}
+                        costDesc={c.costDesc}
+                        costAmount={c.costAmount}
+                        key={k}
+                    />)
+                }
+            </Card>
+        </div>
+    );
 }
 
 export default App;
